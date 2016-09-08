ece59578150c6cb986f556b1e935cad247f5449b
sdroyds <mikew8978@gmail.com>
2016-09-07 23:05:33 -0400

    Lighten the load

--- target/product/core.mk
+++ target/product/core.mk
@@ -24,16 +24,12 @@ PRODUCT_PACKAGES += \
     BasicDreams \
     BlockedNumberProvider \
     BookmarkProvider \
-    Browser2 \
-    Calendar \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
     Contacts \
-    DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
     ExactCalculator \
     ExternalStorageProvider \
     FusedLocation \
